d4a5b0b482ce6d9ec1c14ea6ccdd17aaea6d87ff
herasymenko <herasymenko@zfort.com>
2022-04-03 02:01:58 +0300

    first commit

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+  const port: number = parseInt(`${process.env.PORT}`) || 3000;
   const config = new DocumentBuilder()
     .setTitle('Dogs')
     .setDescription('Dogs app API')
@@ -13,7 +14,7 @@ const bootstrap = async () => {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(process.env.PORT || 4200);
+  await app.listen(port);
 };
 
 bootstrap();
